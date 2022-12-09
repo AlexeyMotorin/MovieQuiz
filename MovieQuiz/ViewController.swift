bf11d0a1e6a7e65707908558ba3df3d982f5ff8e
Motorin Aleksey <aleksey.motorin2015@yandex.ru>
2022-12-09 09:04:12 +0300

    Initial Commit

---
+++ MovieQuiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieQuiz
+//
+//  Created by Алексей Моторин on 09.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
